581d3fb8952222734e3e9a7263db8eaa7f75e3cb
archieparedes <archieparedes@gmail.com>
2018-10-21 02:12:28 -0500

    Prototype 21OCT2018

---
+++ tweetHashtagAnalysis.py
@@ -0,0 +1,24 @@
+import tweepy
+
+from config import consumer_key, consumer_secret, access_token, access_token_secret
+from pprint import pprint
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+def hashAnalysis(hashtag):
+    data = []
+    for tweet in tweepy.Cursor(api.search, q = hashtag, rpp = 100).items(100):
+        try: # this handles unknown characters. i.e emojis
+            if(tweet.text[:2] == 'RT'): # eliminates third part retweet application 
+                pass
+            else:
+                data.append(tweet.text)
+        except: 
+            pass
+
+    return data # returns a list
+
+
